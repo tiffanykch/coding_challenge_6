72e4024e031ac591b37c3002251e2b25b775bb86
Tiffany Kuo Chang <tiffanyk104@usf.edu>
2025-02-09 18:00:36 -0500

    Task 3 - Employee Bonus Calculation

--- cc_6.js
+++ cc_6.js
@@ -22,3 +22,23 @@ console.log(`Sales Tax: $${Math.floor(calculateSalesTax(100, 0.07))}`)
 //Use Math.floor to avoid excess decimals
 
 console.log(`Sales Tax: $${calculateSalesTax(500, 0.1)}`)
+
+//TASK 3: ARROW FUNCTION - EMPLOYEE BONUS CALCULATION
+
+//Create arrow function that calculates employee bonus using salary and performance rating
+const calculateBonus = (salary, performanceRating) => {
+    if (performanceRating === "Excellent") {
+        bonus = salary * 0.2;
+    } else if (performanceRating === "Good") {
+        bonus = salary * 0.1;
+    } else if (performanceRating === "Average") {
+        bonus = salary * 0.05
+    }
+
+    return bonus
+};
+
+//Testing function using data provided and logging output to the console
+console.log(`Bonus: $${calculateBonus(5000, "Excellent")}`);
+
+console.log(`Bonus: $${calculateBonus(7000, "Good")}`)
