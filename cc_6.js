63fa695415db4fbd00cbd8521e48d225a2aed7b1
Tiffany Kuo Chang <tiffanyk104@usf.edu>
2025-02-09 17:45:56 -0500

    Task 2 - Sales Tax Computation

--- cc_6.js
+++ cc_6.js
@@ -1,6 +1,6 @@
 //TASK 1: FUNCTION DECLARATION - BUSINESS PROFIT CALCULATION
 
-//Create functiojn to calculate total profit using Price, COGS, and unit sold
+//Create function that calculate total profit using Price, COGS, and unit sold
 function calculateProfit(costPrice, sellingPrice, unitsSold) {
     return (sellingPrice - costPrice) * unitsSold
 }
@@ -9,3 +9,15 @@ function calculateProfit(costPrice, sellingPrice, unitsSold) {
 console.log(`Total Profit: $${calculateProfit(20, 30, 100)}`);
 
 console.log(`Total Profit: $${calculateProfit(50, 70, 200)}`);
+
+//TASK 2: FUNCTION EXPRESSION - SALES TAX COMPUTATION
+
+//Create function that calculates sales tax using amount and tax rate
+function calculateSalesTax(amount, taxRate) {
+    return amount * taxRate
+}
+
+//Testing function using data provided and logging output to the console
+console.log(`Sales Tax: $${Math.floor(calculateSalesTax(100, 0.07))}`)
+
+console.log(`Sales Tax: $${calculateSalesTax(500, 0.1)}`)
